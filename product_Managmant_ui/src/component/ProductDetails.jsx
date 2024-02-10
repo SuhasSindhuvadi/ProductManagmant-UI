b2b7c06ef79ec9406cd19680f10071bc7a372eaf
SuhasSindhuvadi <134517158+SuhasSindhuvadi@users.noreply.github.com>
2024-02-10 13:55:48 +0530

    Add files via upload

---
+++ product_Managmant_ui/src/component/ProductDetails.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import productService from "../service/product.service";
+
+const ProductDetails = () => {
+    const [productList, setProductList] = useState([]);
+    const [msg, setMsg] = useState("");
+    useEffect(() => {
+      init();
+    }, []);
+  
+    const init = () => {
+      productService
+        .getAllProduct()
+        .then((res) => {
+          setProductList(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+  
+    const deleteProduct = (id) => {
+      productService
+        .deleteProduct(id)
+        .then((res) => {
+          setMsg("Delete Sucessfully");
+          init();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+  
+    return (
+      <>
+        <div className="container mt-3">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="card">
+                <div className="card-header fs-3 text-center">
+                  All Product List
+                  {msg && <p className="fs-4 text-center text-success">{msg}</p>}
+                </div>
+  
+                <div className="card-body">
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">Sl No</th>
+                        <th scope="col">Product Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {productList.map((p, num) => (
+                        <tr>
+                          <td>{num + 1}</td>
+                          <td>{p.productName}</td>
+                          <td>{p.description}</td>
+                          <td>{'\u20B9'}{p.price}</td>
+                          <td>{p.quantity}</td>
+                          <td>{p.status}</td>
+                          <td>
+                            <Link to={'editProduct/'+p.id} className="btn btn-sm btn-primary">
+                              Edit
+                            </Link>
+                            <button
+                              onClick={() => deleteProduct(p.id)}
+                              className="btn btn-sm btn-danger ms-1"
+                            >
+                              Delete
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+  export default ProductDetails;
